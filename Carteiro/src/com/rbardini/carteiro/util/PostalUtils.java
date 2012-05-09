e29b9d0c2468eb2aa0a6f575a6822d064c2fa1a8
Rafael Bardini <rafael@rbardini.com>
2012-05-08 22:40:00 -0300

    Update postal status list

--- Carteiro/src/com/rbardini/carteiro/util/PostalUtils.java
+++ Carteiro/src/com/rbardini/carteiro/util/PostalUtils.java
@@ -40,21 +40,26 @@ public final class PostalUtils {
           Status.DESTINATARIO_RECUSOU_SE,
           Status.DESTINATARIO_NAO_APRESENTOU_SE,
           Status.ENDERECO_INSUFICIENTE,
+          Status.MAL_ENDERECADO,
           Status.ENDERECO_INCOMPLETO,
           Status.ENDERECO_INCORRETO,
           Status.ENDERECO_SEM_DISTRIBUICAO,
+          Status.DISTRIBUICAO_NAO_AUTORIZADA,
           Status.LOGRADOURO_IRREGULAR,
           Status.ENDERECO_IRREGULAR,
           Status.NUMERO_INEXISTENTE,
           Status.EMPRESA_SEM_EXPEDIENTE,
           Status.MERCADORIA_AVARIADA,
           Status.DOCUMENTACAO_NAO_FORNECIDA,
+          Status.DESTINATARIO_NAO_APRESENTOU_DOCUMENTACAO,
           Status.OBJETO_FORA_PADRAO,
+          Status.DIMENSOES_IMPOSSIBILITAM_ENTREGA,
           Status.PEDIDO_NAO_SOLICITADO,
           Status.RECUSADO,
           Status.NAO_PROCURADO,
           Status.OBJETO_PERDIDO_ASSALTO_CARTEIRO,
           Status.OBJETO_PERDIDO_ASSALTO_VEICULO,
+          Status.OBJETO_PERDIDO_ASSALTO_UNIDADE,
           Status.OBJETO_RETIDO,
           Status.IMPORTACAO_NAO_AUTORIZADA
       });
@@ -97,21 +102,26 @@ public final class PostalUtils {
     public static final String DESTINATARIO_RECUSOU_SE = "Destinatário recusou-se a receber";
     public static final String DESTINATARIO_NAO_APRESENTOU_SE = "Destinatário não apresentou-se para receber";
     public static final String ENDERECO_INSUFICIENTE = "Endereço insuficiente para entrega";
+    public static final String MAL_ENDERECADO = "Mal endereçado";
     public static final String ENDERECO_INCOMPLETO = "Endereço incompleto - em pesquisa";
     public static final String ENDERECO_INCORRETO = "Endereço incorreto";
     public static final String ENDERECO_SEM_DISTRIBUICAO = "Endereço sem distribuição domiciliária - Entrega interna não autorizada";
+    public static final String DISTRIBUICAO_NAO_AUTORIZADA = "Distribuição não autorizada";
     public static final String LOGRADOURO_IRREGULAR = "Logradouro com numeração irregular";
     public static final String ENDERECO_IRREGULAR = "Endereço com numeração irregular - Em pesquisa";
     public static final String NUMERO_INEXISTENTE = "Não existe o número indicado";
     public static final String EMPRESA_SEM_EXPEDIENTE = "Empresa sem expediente";
     public static final String MERCADORIA_AVARIADA = "Mercadoria avariada";
     public static final String DOCUMENTACAO_NAO_FORNECIDA = "Documentação não fornecida pelo destinatário";
+    public static final String DESTINATARIO_NAO_APRESENTOU_DOCUMENTACAO = "Destinatário não apresentou a documentação";
     public static final String OBJETO_FORA_PADRAO = "Objeto fora do padrão - Limites de dimensão";
+    public static final String DIMENSOES_IMPOSSIBILITAM_ENTREGA = "Dimensões impossibilitam a entrega";
     public static final String PEDIDO_NAO_SOLICITADO = "Pedido não solicitado";
     public static final String RECUSADO = "Recusado";
     public static final String NAO_PROCURADO = "Não procurado";
     public static final String OBJETO_PERDIDO_ASSALTO_CARTEIRO = "Objeto perdido em assalto ao carteiro";
     public static final String OBJETO_PERDIDO_ASSALTO_VEICULO = "Objeto perdido em assalto a veículo dos correios";
+    public static final String OBJETO_PERDIDO_ASSALTO_UNIDADE = "Objeto perdido em assalto a unidade dos correios";
     public static final String OBJETO_RETIDO = "Objeto retido pelo órgão de fiscalização";
     public static final String IMPORTACAO_NAO_AUTORIZADA = "Importação não autorizada por órgão da receita";
 
@@ -137,8 +147,10 @@ public final class PostalUtils {
     public static final String ENTREGA_PROGRAMADA = "Entrega programada";
     public static final String DISTRIBUICAO_ESPECIAL_AGENDADA = "Distribuição especial agendada";
     public static final String AGUARDANDO_PARTE_LOTE = "Aguardando parte do lote";
+    public static final String ENTREGA_CONDICIONADA_COMPOSICAO_LOTE = "Entrega condicionada à composição do lote";
     public static final String ATRASADO = "Atrasado";
     public static final String LOG_REVERSA_SIMULTANEA = "Log. reversa simultânea";
+    public static final String LOGISTICA_REVERSA_SIMULTANEA = "Logística reversa simultânea";
     public static final String REINTEGRADO = "Reintegrado";
     public static final String DEVOLVIDO_AOS_CORREIOS = "Objeto devolvido aos correios";
     public static final String SAIDA_CANCELADA = "Saída cancelada";
@@ -173,21 +185,26 @@ public final class PostalUtils {
       map.put(DESTINATARIO_RECUSOU_SE, Category.IRREGULAR);
       map.put(DESTINATARIO_NAO_APRESENTOU_SE, Category.IRREGULAR);
       map.put(ENDERECO_INSUFICIENTE, Category.IRREGULAR);
+      map.put(MAL_ENDERECADO, Category.IRREGULAR);
       map.put(ENDERECO_INCOMPLETO, Category.IRREGULAR);
       map.put(ENDERECO_INCORRETO, Category.IRREGULAR);
-      map.put(ENDERECO_SEM_DISTRIBUICAO , Category.IRREGULAR);
+      map.put(ENDERECO_SEM_DISTRIBUICAO, Category.IRREGULAR);
+      map.put(DISTRIBUICAO_NAO_AUTORIZADA, Category.IRREGULAR);
       map.put(LOGRADOURO_IRREGULAR, Category.IRREGULAR);
       map.put(ENDERECO_IRREGULAR, Category.IRREGULAR);
       map.put(NUMERO_INEXISTENTE, Category.IRREGULAR);
       map.put(EMPRESA_SEM_EXPEDIENTE, Category.IRREGULAR);
       map.put(MERCADORIA_AVARIADA, Category.IRREGULAR);
       map.put(DOCUMENTACAO_NAO_FORNECIDA, Category.IRREGULAR);
+      map.put(DESTINATARIO_NAO_APRESENTOU_DOCUMENTACAO, Category.IRREGULAR);
       map.put(OBJETO_FORA_PADRAO, Category.IRREGULAR);
+      map.put(DIMENSOES_IMPOSSIBILITAM_ENTREGA, Category.IRREGULAR);
       map.put(PEDIDO_NAO_SOLICITADO, Category.IRREGULAR);
       map.put(RECUSADO, Category.IRREGULAR);
       map.put(NAO_PROCURADO, Category.IRREGULAR);
       map.put(OBJETO_PERDIDO_ASSALTO_CARTEIRO, Category.IRREGULAR);
       map.put(OBJETO_PERDIDO_ASSALTO_VEICULO, Category.IRREGULAR);
+      map.put(OBJETO_PERDIDO_ASSALTO_UNIDADE, Category.IRREGULAR);
       map.put(OBJETO_RETIDO, Category.IRREGULAR);
       map.put(IMPORTACAO_NAO_AUTORIZADA, Category.IRREGULAR);
 
@@ -226,20 +243,25 @@ public final class PostalUtils {
       map.put(DESTINATARIO_RECUSOU_SE, R.drawable.ic_postal_recusado);
       map.put(DESTINATARIO_NAO_APRESENTOU_SE, R.drawable.ic_postal_nao_procurado);
       map.put(ENDERECO_INSUFICIENTE, R.drawable.ic_postal_endereco_insuficiente);
+      map.put(MAL_ENDERECADO, R.drawable.ic_postal_endereco_insuficiente);
       map.put(ENDERECO_INCOMPLETO, R.drawable.ic_postal_endereco_insuficiente);
       map.put(ENDERECO_INCORRETO, R.drawable.ic_postal_endereco_incorreto);
-      map.put(ENDERECO_SEM_DISTRIBUICAO , R.drawable.ic_postal_endereco_sem_distribuicao);
+      map.put(ENDERECO_SEM_DISTRIBUICAO, R.drawable.ic_postal_endereco_sem_distribuicao);
+      map.put(DISTRIBUICAO_NAO_AUTORIZADA, R.drawable.ic_postal_endereco_sem_distribuicao);
       map.put(LOGRADOURO_IRREGULAR, R.drawable.ic_postal_endereco_insuficiente);
       map.put(ENDERECO_IRREGULAR, R.drawable.ic_postal_endereco_insuficiente);
       map.put(NUMERO_INEXISTENTE, R.drawable.ic_postal_endereco_incorreto);
       map.put(EMPRESA_SEM_EXPEDIENTE, R.drawable.ic_postal_empresa_sem_expediente);
       map.put(DOCUMENTACAO_NAO_FORNECIDA, R.drawable.ic_postal_documentacao_nao_fornecida);
+      map.put(DESTINATARIO_NAO_APRESENTOU_DOCUMENTACAO, R.drawable.ic_postal_documentacao_nao_fornecida);
       map.put(OBJETO_FORA_PADRAO, R.drawable.ic_postal_objeto_fora_padrao);
+      map.put(DIMENSOES_IMPOSSIBILITAM_ENTREGA, R.drawable.ic_postal_objeto_fora_padrao);
       map.put(PEDIDO_NAO_SOLICITADO, R.drawable.ic_postal_pedido_nao_solicitado);
       map.put(RECUSADO, R.drawable.ic_postal_recusado);
       map.put(NAO_PROCURADO, R.drawable.ic_postal_nao_procurado);
       map.put(OBJETO_PERDIDO_ASSALTO_CARTEIRO, R.drawable.ic_postal_objeto_perdido_assalto_carteiro);
       map.put(OBJETO_PERDIDO_ASSALTO_VEICULO, R.drawable.ic_postal_objeto_perdido_assalto_veiculo);
+      map.put(OBJETO_PERDIDO_ASSALTO_UNIDADE, R.drawable.ic_postal_objeto_perdido_assalto_unidade);
       map.put(OBJETO_RETIDO, R.drawable.ic_postal_objeto_retido);
       map.put(IMPORTACAO_NAO_AUTORIZADA, R.drawable.ic_postal_objeto_retido);
 
@@ -263,8 +285,10 @@ public final class PostalUtils {
       map.put(MAL_ENCAMINHADO, R.drawable.ic_postal_mal_encaminhado);
       map.put(DESTINATARIO_AUSENTE, R.drawable.ic_postal_destinatario_ausente);
       map.put(AGUARDANDO_PARTE_LOTE, R.drawable.ic_postal_aguardando_parte_lote);
+      map.put(ENTREGA_CONDICIONADA_COMPOSICAO_LOTE, R.drawable.ic_postal_aguardando_parte_lote);
       map.put(ATRASADO, R.drawable.ic_postal_atrasado);
       map.put(LOG_REVERSA_SIMULTANEA, R.drawable.ic_postal_log_reversa_simultanea);
+      map.put(LOGISTICA_REVERSA_SIMULTANEA, R.drawable.ic_postal_log_reversa_simultanea);
       map.put(REINTEGRADO, R.drawable.ic_postal_reintegrado);
       map.put(DEVOLVIDO_AOS_CORREIOS, R.drawable.ic_postal_reintegrado);
       map.put(SAIDA_CANCELADA, R.drawable.ic_postal_saida_cancelada);
@@ -274,7 +298,6 @@ public final class PostalUtils {
       map.put(ENTREGA_NAO_EFETUADA, R.drawable.ic_postal_entrega_nao_efetuada);
       map.put(ENTREGA_NAO_EFETUADA_MOTIVOS_OPERACIONAIS, R.drawable.ic_postal_entrega_nao_efetuada);
       map.put(EMPRESA_SEM_EXPEDIENTE, R.drawable.ic_postal_empresa_sem_expediente);
-      map.put(DOCUMENTACAO_NAO_FORNECIDA, R.drawable.ic_postal_documentacao_nao_fornecida);
       map.put(AGUARDANDO_DOCUMENTACAO_FISCAL, R.drawable.ic_postal_aguardando_documentacao_fiscal);
       map.put(ENTREGA_CONDICIONADA_COMPLEMENTO_DOCUMENTACAO, R.drawable.ic_postal_aguardando_documentacao_fiscal);
       map.put(CANCELADA_ULTIMA_INFORMACAO, R.drawable.ic_postal_cancelada_ultima_informacao);
