0f34d5d97b6626141d416fc969b9287c4aa0468d
Rafael Bardini <rafael@rbardini.com>
2014-01-13 21:06:52 -0200

    Remove license check

--- Carteiro/src/com/rbardini/carteiro/ui/MainActivity.java
+++ Carteiro/src/com/rbardini/carteiro/ui/MainActivity.java
@@ -7,24 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.Settings.Secure;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.util.Log;
 import android.view.View;
 import android.widget.SearchView;
-
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.widget.ShareActionProvider;
-import com.google.android.vending.licensing.AESObfuscator;
-import com.google.android.vending.licensing.LicenseChecker;
-import com.google.android.vending.licensing.LicenseCheckerCallback;
-import com.google.android.vending.licensing.Policy;
-import com.google.android.vending.licensing.ServerManagedPolicy;
 import com.rbardini.carteiro.CarteiroApplication;
 import com.rbardini.carteiro.R;
 import com.rbardini.carteiro.model.PostalItem;
@@ -39,21 +30,13 @@ public class MainActivity extends SherlockFragmentActivity implements Detachable
   protected static final String TAG = "MainActivity";
   public static final int ADD_REQUEST = 1;
 
-  private static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAubBLpYTT5EF9nwTS72jP5ZLZK+ABgxEVssPr7bfQ1QnL3b/EDBAWEuRFaT34ZPU1yffZ6pCE+8AI5EWFsxIE679dev28gN0yJasgsouoeWdHpM5ZojNyjY7POzockXGgVervGTCZENEtMNuhDDKr1136vuKvKtC91e844r07Zyug0Vs2ye26ZnbKXk2pgnpwmwaop1ejqr5kYED5BDD55mgMY3/7HjwUf2k8IK7PUYUcd45z0CjVhSSYQtctAPBVffhcTd1XZQIf/6MvWFSLLCmf+q/r3B6hVEARTEluR23PtElPgaJkBeBcwJM4TbTIgPWq9V2FDSV5IeoRKnazqwIDAQAB";
-  private static final byte[] SALT = new byte[] {74, 73, 116, -105, 115, 29, -59, -53, -11, 105, -75, 117, 76, 15, 82, -50, 71, -109, 125, -50};
-
   private CarteiroApplication app;
   private FragmentManager fragManager;
   private MainPagerAdapter pagerAdapter;
   private ShareActionProvider mShareActionProvider;
 
-  private Handler handler;
-  private LicenseCheckerCallback licenseCheckerCallback;
-    private LicenseChecker licenseChecker;
-    private boolean licenseChecked;
-
   @Override
-    public void onCreate(Bundle savedInstanceState) {
+  public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
@@ -67,13 +50,13 @@ public class MainActivity extends SherlockFragmentActivity implements Detachable
     });
 
     ViewPager viewPager = (ViewPager) findViewById(R.id.postal_list_pager);
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.setOffscreenPageLimit(3);
-        viewPager.setCurrentItem(3);
+    viewPager.setAdapter(pagerAdapter);
+    viewPager.setOffscreenPageLimit(3);
+    viewPager.setCurrentItem(3);
 
-        TitlePageIndicator indicator = (TitlePageIndicator) findViewById(R.id.indicator);
-        indicator.setViewPager(viewPager);
-        indicator.setOnPageChangeListener(new OnPageChangeListener() {
+    TitlePageIndicator indicator = (TitlePageIndicator) findViewById(R.id.indicator);
+    indicator.setViewPager(viewPager);
+    indicator.setOnPageChangeListener(new OnPageChangeListener() {
       @Override
       public void onPageSelected(int position) { updateRefreshStatus(); }
 
@@ -83,25 +66,11 @@ public class MainActivity extends SherlockFragmentActivity implements Detachable
       @Override
       public void onPageScrollStateChanged(int state) {}
     });
-
-        String deviceId = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
-        handler = new Handler();
-        licenseCheckerCallback = new CarteiroLicenseCheckerCallback();
-        licenseChecker = new LicenseChecker(this, new ServerManagedPolicy(this, new AESObfuscator(SALT, getPackageName(), deviceId)), PUBLIC_KEY);
-        try {
-          licenseChecked = (Boolean) getLastCustomNonConfigurationInstance();
-        } catch (NullPointerException e) {
-          licenseChecked = false;
-        }
   }
 
   @Override
   protected void onResume() {
     super.onResume();
-
-    if (!licenseChecked && fragManager.findFragmentByTag(MainDialogFragment.TAG) == null) {
-      checkLicense();
-    }
 
     CarteiroApplication.state.receiver.setReceiver(this);
     updateRefreshStatus();
@@ -120,17 +89,6 @@ public class MainActivity extends SherlockFragmentActivity implements Detachable
         }
     CarteiroApplication.state.receiver.clearReceiver();
   }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    licenseChecker.onDestroy();
-  }
-
-  @Override
-    public Object onRetainCustomNonConfigurationInstance() {
-      return licenseChecked;
-    }
 
   @Override
   public void onReceiveResult(int resultCode, Bundle resultData) {
@@ -255,45 +213,4 @@ public class MainActivity extends SherlockFragmentActivity implements Detachable
     pagerAdapter.notifyDataSetChanged();
     setShareIntent();
   }
-
-  protected void checkLicense() {
-        licenseChecker.checkAccess(licenseCheckerCallback);
-    }
-
-    private void licenseChecked(final boolean licensed, final boolean retry) {
-        handler.post(new Runnable() {
-            @Override
-      public void run() {
-              try {
-                if (!licensed) {
-                  MainDialogFragment.newInstance(R.id.license_opt, retry).show(getSupportFragmentManager(), MainDialogFragment.TAG);
-                }
-                licenseChecked = true;
-              } catch (Exception e) { Log.i(TAG, "Dropping license response."); }
-            }
-        });
-    }
-
-  private class CarteiroLicenseCheckerCallback implements LicenseCheckerCallback {
-        private static final String TAG = "CarteiroLicenseCheckerCallback";
-
-    @Override
-    public void allow(int policyReason) {
-            if (isFinishing()) { return; }
-            licenseChecked(true, false);
-        }
-
-        @Override
-    public void dontAllow(int policyReason) {
-            if (isFinishing()) { return; }
-            licenseChecked(false, policyReason == Policy.RETRY);
-        }
-
-        @Override
-    public void applicationError(int errorCode) {
-          Log.w(TAG, "License check application error: "+errorCode);
-            if (isFinishing()) { return; }
-            licenseChecked(true, false);
-        }
-    }
 }
