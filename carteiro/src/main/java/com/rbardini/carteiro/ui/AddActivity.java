fe37c7c368a339c7294f73201bc50dd2fd65f52b
Rafael Bardini <rafael@rbardini.com>
2016-02-08 23:00:53 -0200

    Unhide barcode scanner option in AddActivity

--- carteiro/src/main/java/com/rbardini/carteiro/ui/AddActivity.java
+++ carteiro/src/main/java/com/rbardini/carteiro/ui/AddActivity.java
@@ -3,10 +3,12 @@ package com.rbardini.carteiro.ui;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -166,6 +168,13 @@ public class AddActivity extends AppCompatActivity {
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.add_actions, menu);
+
+    MenuItem scanItem = menu.findItem(R.id.scan_opt);
+    Drawable scanIcon = DrawableCompat.wrap(scanItem.getIcon());
+
+    DrawableCompat.setTint(scanIcon, getResources().getColor(R.color.text_primary));
+    scanItem.setIcon(scanIcon);
+
     return super.onCreateOptionsMenu(menu);
   }
 
